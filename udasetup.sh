72645a3295d200c1daafb516ada97d5228f1a41c
Chris Rowe <rwbotx@gmail.com>
2019-01-12 23:48:06 -0500

    update aliases

--- udasetup.sh
+++ udasetup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+# Call from .student_bashrc
+# 		source /home/workspace/RoboND-DeepRL-Project/udasetup.sh
 # Manually source this file using:
-#     sudo source /home/workspace/RoboND-DeepRL-Project/udasetup.sh
+#		sudo -s source /home/workspace/RoboND-DeepRL-Project/udasetup.sh
 
-sudo apt-get install libignition-math2-dev
+#alias apm="sudo apt-get install libignition-math2-dev"
 
 alias gitcon="git config --global credential.helper 'cache --timeout=999999'; git config --global user.name 'rwbot'; git config --global user.email 'rwbotx@gmail.com'"
 
@@ -11,6 +13,7 @@ alias cdcb="cd /home/workspace/RoboND-DeepRL-Project/build"
 alias cdcbz="cd /home/workspace/RoboND-DeepRL-Project/build/x86_64/bin"
 alias cdcmk="cdcb; make" #goes to catkin_ws, runs catkin_make, waits until it's made, then sources devel/setup.bash
 alias rr="cdcbz; ./gazebo-arm.sh"
+alias rro="cdcbz; ./gazebo-arm.sh >> /home/workspace/RoboND-DeepRL-Project/output.txt"
 
 alias gcm="cdc; git checkout master"
 alias gcw="cdc; git checkout workspaces"
